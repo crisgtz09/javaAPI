6350dd2af337a25664908c632a43e81b43ebc47d
crisgtz09 <55902849+crisgtz09@users.noreply.github.com>
2022-08-20 10:10:28 -0600

    Add files via upload

--- usuarios.js
+++ usuarios.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $('#usuarios').DataTable();
 });
 
-
+//----->metodo GET<-----
 async function cargarUsuario() {
   const request = await fetch('api/usuarios', {
     method: 'GET',
@@ -16,7 +16,7 @@ async function cargarUsuario() {
   const usuarios = await request.json();
 
 
-
+//----->se declara el listado para que vaya metiendo los datos que obtiene de la API<-----
   let listadoHtml = '';
   for (let usuario of usuarios) {
     let botonEliminar = '<a href="#" onclick="eliminarUsuario(' + usuario.id + ')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
@@ -27,16 +27,19 @@ async function cargarUsuario() {
                         + '</td><td>'+usuario.password+'</td><td>' + botonEliminar + '</td></tr>'
     listadoHtml+= usuarioHtml;
     }
-
+//----> se selecciona el campo donde se va a inyectar para la tabla<-----
 document.querySelector('#Usuarios tbody').outerHTML = listadoHtml;
 }
-
+//------>pregunta de confirmacion se asigna como falso para que no se
+//------>borre aun si se reponde que no
 async function eliminarUsuario(id){
 
   if (!confirm('¿Desea eliminar el usuario?')){
-
+//---->Se mete un return para que se corte ahi la ejecutacion de codigo
+//---->mientras se mantenga en "False"
     return;
   }
+//-----> metodo delete que se le asigna al boton<-----
   const request = await fetch('api/usuarios/' + id, {
     method: 'Delete',
     headers: {
