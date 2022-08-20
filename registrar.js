d722cf7e21960050b32505a3b2beea09efdf3fe1
crisgtz09 <55902849+crisgtz09@users.noreply.github.com>
2022-08-20 10:05:46 -0600

    Add files via upload

--- registrar.js
+++ registrar.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
     //On Ready
 });
+// -----> se le pone el "async" porque abajo en el fetch tiene "await"<-----
 async function registrarUsuario() {
     let datos = {};
-
+//------>se seleccionan los datos mediante el id en el html<-----
     datos.nombre = document.getElementById('txtNombre').value;
     datos.apellido = document.getElementById('txtApellido').value;
     datos.telefono = document.getElementById('txtTelefono').value;
     datos.email = document.getElementById('txtEmail').value;
     datos.password = document.getElementById('txtPassword').value;
-
+//----->la contraseña repetida se asigna como variable para comprobar que es la misma<-----
     let repetirPassword = document.getElementById('txtRepetirPassword').value;
-
+//----->mediante un if se comprueba y se manda alerta en caso de que no<-----
     if (repetirPassword != datos.password){
         alert("la contraseña que escribiste es diferente");
     }
+    //----------->metodo<-------------
     const request = await fetch('api/usuarios', {
         method: 'POST',
         headers: {
@@ -22,7 +24,7 @@ async function registrarUsuario() {
           'Content-Type': 'application/json'
         },
     
-    
+    //----->Stringify se encarga de convertir los datos a JSON para mandarlos<-----
     body: JSON.stringify(datos)
     });
     const usuario = await request.json();
